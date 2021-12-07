d356ead5d35f7523cae554d110be0575fe368811
HiepSi2323 <38238584+HiepSi2323@users.noreply.github.com>
2021-12-07 08:49:15 +0700

    Update convertXlsxTo2D.js

--- exercise/convertXlsxTo2D.js
+++ exercise/convertXlsxTo2D.js
@@ -10,13 +10,13 @@ const data = xlsx.utils.sheet_to_json(readSheetExcelFile);
 
 // convert json to 2d array
 function jsonTo2D(arrayOfObj){
-    let arraykeys = []; 
+    let arraykeys = ['id', 'no', 'a', 'b', 'c', 'd'];
     let arrayNested = [];
 
     arrayOfObj.forEach(obj => {
         // array key 
-        Object.keys(obj).forEach(key => arraykeys.includes(key) || arraykeys.push(key));
-        let row = new Array(arraykeys.length); // has 19 keys 
+        Object.keys(obj).forEach(key => arraykeys.includes(key));
+        let row = new Array(arraykeys.length); // load 6 keys 
         arraykeys.forEach((col,i) => row[i] = obj[col]); // get values 
         arrayNested.push(row); 
     });
